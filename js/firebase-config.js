14eb103e295686fdf32e3c4e480ac305789b5811
ahmedrashadali7-sketch <ahmedrashadali7@gmail.com>
2026-03-02 20:35:00 +0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,7 @@
-// استيراد مكتبات Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// إعدادات Firebase - باستخدام المفتاح الجديد
 const firebaseConfig = {
     apiKey: "AIzaSyDpuVMALR_-vDdvCQFqWuYulioPWXm49Kg",
     authDomain: "shaghaf-story.firebaseapp.com",
@@ -13,10 +11,8 @@ const firebaseConfig = {
     appId: "1:197591948575:web:a0aaee7b7971f100c4e42a"
 };
 
-// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// تصدير المتغيرات لاستخدامها في باقي الملفات
 export { app, auth, db };
